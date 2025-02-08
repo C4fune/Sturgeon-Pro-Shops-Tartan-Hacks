4dfbbdfe50ce7e64435b3c9fb872d595a57cb416
Seunghyeok Lee <shstevelee05@gmail.com>
2025-02-08 13:21:46 -0500

    reorganized bookshelf

--- frontend/mywriter/src/app/account/bookshelf/page.tsx
+++ frontend/mywriter/src/app/account/bookshelf/page.tsx
@@ -44,41 +44,35 @@ export default function BookshelfPage() {
 
   return (
     <main className="min-h-screen p-8 sm:p-20 font-robotoMono bg-eggshell">
-      <h1 className="text-3xl mb-8 font-bold">My BookShelf</h1>
+      <h1 className="text-3xl mb-8">My BookShelf</h1>
       <button
-        className="bg-black rounded hover:bg-eggshell hover:text-black border-transparent hover:border-battleship border-2 text-white px-3 py-1 mb-4"
+        className="bg-black hover:bg-eggshell hover:text-black border-battleship text-white px-3 py-1 mb-4"
         onClick={returnToHome}
       >
         Return to Home
       </button>
 
       <div className="grid grid-cols-24 grid-rows-2 gap-4">
-        <div className="flex gap-4">
-          <div
-            className="rounded-md hover:bg-white shadow-sm flex-1 flex items-center justify-center text-center h-40 cursor-pointer bg-accept"
-            onClick={handlePlusClick}
-          >
-            <p className="lg:text-4xl md:text-3xl text-xl font-bold">
-              Write a new book
-            </p>
-          </div>
-          <div
-            className="rounded-md hover:bg-white shadow-sm flex-1 flex items-center justify-center text-center h-40 cursor-pointer bg-accept"
-            onClick={handlePlusClick}
-          >
-            <p className="lg:text-4xl md:text-3xl text-xl font-bold">
-              Upload a book
-            </p>
-          </div>
-        </div>
         {books.map((book, i) => (
           <div
             key={i}
-            className="border-battleship rounded-md border-4 bg-battleship shadow-sm flex items-center justify-center text-center h-40 hover:bg-eggshell"
+            className="border-battleship border-4 bg-battleship shadow-sm flex items-center justify-center text-center h-40 hover:bg-eggshell"
             onClick={handlePlusClick}
           >
             <div className="text-4xl text-black">{book.title} </div>
           </div>
+        ))}
+        <div
+          className="border border-battleship bg-white shadow-sm flex items-center justify-center text-center h-40 cursor-pointer hover:bg-accept"
+          onClick={handlePlusClick}
+        >
+          <p className="text-6xl font-bold">+</p>
+        </div>
+        {Array.from({ length: 1 }, (_, i) => (
+          <div
+            key={`filler_${i}`}
+            className="border border-battleship bg-gray-50 h-40"
+          />
         ))}
       </div>
     </main>
