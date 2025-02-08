f7215ad14f550036d05b54037c6a1774629a30d0
Joe Cardenas <coderjoe05@gmail.com>
2025-02-08 00:11:29 -0500

    mostly done with onboarding

---
+++ frontend/mywriter/src/app/Components/LoadingBar.tsx
@@ -0,0 +1,16 @@
+export default function LoadingBar({ progress }: { progress: number }) {
+    return (
+        <div className="w-full bg-slate-200 rounded-full h-2.5">
+            <div 
+                className="bg-indigo-600 h-2.5 rounded-full transition-all duration-300 ease-out" 
+                style={{ width: `${progress}%` }}
+            >
+                <div className="w-full text-right pr-1 -mt-5">
+                    <span className="text-xs font-medium text-indigo-600">
+                        {Math.round(progress)}%
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
+}
