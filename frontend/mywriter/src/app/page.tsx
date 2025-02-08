25675586cb0ac2e91abb4e71b0ad77e0adede18f
Seunghyeok Lee <shstevelee05@gmail.com>
2025-02-08 00:43:11 -0500

    small color fix with the button

--- frontend/mywriter/src/app/page.tsx
+++ frontend/mywriter/src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
         </p>
         <div className="flex justify-center space-x-4">
           <LoginButton />
-          <button className="bgdark text-eggshell px-4 py-2 rounded-lg hover:bg-gray-700">
+          <button className="bg-black text-eggshell px-4 py-2 rounded-lg hover:bg-battleship">
             <a href="https://docs.google.com/presentation/d/1lMtLE8DsY-DJqViKbghTCm6WyT0A7UI5MA8Kp7G8JcI/edit?usp=sharing">
               Learn More
             </a>
